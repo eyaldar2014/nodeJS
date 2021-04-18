75d4787f942738d495e87bc421c252fa6783e39e
Eyal Dar <eyaldar2014@gmail.com>
2021-04-18 13:03:14 +0300

    1.1

---
+++ nodeJS/exercises/1.1/nodeVSjs.js
@@ -0,0 +1,9 @@
+// the essence of node is made for the server 
+// the essence of js (client) is for the web browser
+
+// different eco system -
+
+// node is entirely contorolled environment compared to client js interface
+// node is faster because its has only specif use and expertice. its localStorage(un-dependent)
+// node has: "global", "process", "fs...."
+// client js has: "window", "document", all dom orders...
